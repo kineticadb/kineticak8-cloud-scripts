18c2216d7f4cba1be3efdffcf2db65ccb7b66fb8
Franz <5334409+franz-net@users.noreply.github.com>
2020-10-21 10:03:28 -0400

    Update azureDeployment.sh

--- azureDeployment.sh
+++ azureDeployment.sh
@@ -545,9 +545,9 @@ deployKineticaCluster
 
 ## Setting up default backup schedules
 #weekly retain 30 days
-velero schedule create default-gpudb-backup-weekly --schedule "@every 168h" --include-namespaces gpudb --ttl 720h0m0s
+#velero schedule create default-gpudb-backup-weekly --schedule "@every 168h" --include-namespaces gpudb --ttl 720h0m0s
 #daily retain 8 days
-velero schedule create default-gpudb-backup-daily --schedule "@every 24h" --include-namespaces gpudb --ttl 192h0m0s
+#velero schedule create default-gpudb-backup-daily --schedule "@every 24h" --include-namespaces gpudb --ttl 192h0m0s
 
 #checkForKineticaRanksReadiness
 
