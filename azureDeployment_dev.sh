24e826391091f0691e107c22fef3b3212bd06fbf
Franz <5334409+franz-net@users.noreply.github.com>
2020-12-11 14:39:36 -0500

    Update azureDeployment_dev.sh

--- azureDeployment_dev.sh
+++ azureDeployment_dev.sh
@@ -364,7 +364,7 @@ spec:
     image: kinetica/kinetica-k8s-intel:v7.1.1
     clusterName: "$kcluster_name"
     config:
-        tieredStorageConfig:
+        tieredStorage:
           persistTier:
             default:  
               volumeClaim:
